186e551491f12ce0db616b9298757a065caf8ed5
David <63363958+DavidRyab@users.noreply.github.com>
2023-05-23 18:17:06 +0300

    Update page.tsx

--- src/app/iframe/page.tsx
+++ src/app/iframe/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return(
-        <iframe src="https://urchin-app-lenyu.ondigitalocean.app/" style={{height: "185px", width: "865px", border: "none" }}></iframe>
+        <iframe src="https://urchin-app-lenyu.ondigitalocean.app/"></iframe>
     )
 }
